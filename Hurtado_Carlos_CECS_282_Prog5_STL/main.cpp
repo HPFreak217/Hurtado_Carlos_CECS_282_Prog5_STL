97ea2a468d27d3d425f12071a0c0b9add71b0725
Carlos Hurtado <Carlos@dhcp-39-100-140.dhcpint.csulb.edu>
2016-11-16 10:21:18 -0800

    Initial Commit

---
+++ Hurtado_Carlos_CECS_282_Prog5_STL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hurtado_Carlos_CECS_282_Prog5_STL
+//
+//  Created by Carlos Hurtado on 11/16/16.
+//  Copyright © 2016 Carlos Hurtado. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
